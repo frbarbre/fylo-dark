421dc7118b2c2e5189358513094414746b34d3ea
frbarbre <fr.barbre@gmail.com>
2023-04-27 23:12:57 +0200

    update

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -7,7 +7,7 @@ const CTA = () => {
       <h2 className={`${styles.heading}`}>Get early access today</h2>
       <p className={`${styles.paragraphSm} text-gray-300 max-w-[650px]`}>It only takes a minute to sign up and our free starter tier is extremely generous. If you have any questions, our support team would be happy to help you.</p>
       <div className="flex flex-col items-center lg:flex-row w-[80%] gap-6">
-        <input placeholder="email@example.com" type="text" className="w-[100%] bg-white h-[50px] rounded-full text-lightgray ring-lightblue indent-3 xs:indent-8 drop-shadow" />
+        <input placeholder="email@example.com" type="text" className="w-[100%] bg-white h-[50px] rounded-full text-lightgray ring-lightblue text-center xs:text-left xs:indent-8 drop-shadow" />
         <Button textStyles={"Get Started For Free"} />
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav className={`${styles.padding} flex flex-wrap gap-12 justify-center xs:justify-between items-center max-2-[1400px] mx-auto`}>
       <img src={Logo} alt="logo" className="w-[120px] object-contain" />
-      <div className={`${styles.flexCenter} gap-6 md:gap-12`}>
+      <div className={`flex items-center justify-around w-[100%] xs:w-auto gap-6 md:gap-12`}>
         {navItems.map((item) => (
           <a key={item.id} className="hover:underline text-gray-300 hover:text-white hover:font-semibold transition-all" href="">{item.title}</a>
         ))}
